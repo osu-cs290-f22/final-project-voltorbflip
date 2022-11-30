8de930c7ee764cf86edcde02260fa87d3e655f9e
Lucas Dunn <dunnlu@oregonstate.edu>
2022-11-29 19:50:05 -0800

    Added name input modal to HTML

--- public/index.js
+++ public/index.js
@@ -4,22 +4,48 @@ Written by: Lucas Dunn and Garrett Biwer
 */
 var timerOn = false
 
+function uploadTime(time) {
+    
+
+
+
+}
+
+
+
+
+
 function startTimer() {
     var timer = document.getElementById("Time")
     var sec = 0
     var min = 0
     var timerCount = setInterval(function () {
-        if (timerOn === 'false')
+        if (!timerOn)
             clearInterval(timerCount)
         sec++
         if (sec === 60) {
             sec = 0
             min++ 
         }
-        timer.textContent = min + ':' + sec
+        if (sec<10) {
+            timer.textContent = min + ':0' + sec
+        } else {
+            timer.textContent = min + ':' + sec
+        }
+
+        
     }, 1000)
     var time = {min,sec}
-    return time
+    uploadTime(time)
 }
 
 
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    var startTimerButton = document.getElementById('start-timer-button')
+    startTimerButton.addEventListener('click', startTimer)
+
+
+
+})
