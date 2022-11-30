daacb328b64df0ad48f0e2b341add0ffc372700f
Lucas Dunn <dunnlu@oregonstate.edu>
2022-11-29 19:19:11 -0800

    First implementation of timer

--- public/index.js
+++ public/index.js
@@ -2,3 +2,24 @@
 This is the index javascript for Voltorb Flip Speedrun
 Written by: Lucas Dunn and Garrett Biwer
 */
+var timerOn = false
+
+function startTimer() {
+    var timer = document.getElementById("Time")
+    var sec = 0
+    var min = 0
+    var timerCount = setInterval(function () {
+        if (timerOn === 'false')
+            clearInterval(timerCount)
+        sec++
+        if (sec === 60) {
+            sec = 0
+            min++ 
+        }
+        timer.textContent = min + ':' + sec
+    }, 1000)
+    var time = {min,sec}
+    return time
+}
+
+
