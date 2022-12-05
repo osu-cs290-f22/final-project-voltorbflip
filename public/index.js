8f889e5aedef11dc4ef6f03424245c033e2613ff
biwerg <biwerg@oregonstate.edu>
2022-12-04 17:37:26 -0800

    Added game functionality
    hard coded a board and added functions for the game to run properly

--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ Written by: Lucas Dunn and Garrett Biwer
 */
 var timerOn = false
 var currentTime = {min: 0,sec: 0}
+var total = 0;
 
 function addTime(name,currentTime) {
     var recordInput = {name: name,min: currentTime.min, sec: currentTime.sec}
@@ -102,9 +103,12 @@ function startTimer() {
     var timerCount = setInterval(function () {
         if (!timerOn) {
             currentTime = {min: min,sec: sec}
-            uploadTime()
+            if (total == 9){
+                uploadTime()
+            }
             showControlPanel()
             clearInterval(timerCount)
+            resetBoard();
         }
         sec++
         if (sec === 60) {
@@ -121,8 +125,63 @@ function startTimer() {
     }, 1000)
     
 }
+function resetBoard(){
+    total = 0;
+    var tiles = document.getElementsByClassName('tile-front');
+    for (var i = 0; i < tiles.length; i++) {
+        tiles[i].classList.add("hidden");
+    }
+    tiles = document.getElementsByClassName('tile-back');
+    for (var i = 0; i < tiles.length; i++) {
+        tiles[i].classList.remove("hidden");
+    }
+}
 
-
+const flip = function() {
+    var value;
+    if (timerOn == true){
+        this.parentElement.classList.add("hidden");
+        this.parentElement.previousElementSibling.classList.remove("hidden");
+        value = this.parentElement.parentElement.getAttribute("data-value");
+        console.log(value);
+        if (value == 0){
+            timerOn = false;
+        }
+        else {
+            if (value != 1){
+                total = parseInt(value) + total;
+                if (total == 9){
+                    timerOn = false;
+                }
+            }
+        }
+    }
+}
+document.getElementById("tile-1-back").onclick = flip;
+document.getElementById("tile-2-back").onclick = flip;
+document.getElementById("tile-3-back").onclick = flip;
+document.getElementById("tile-4-back").onclick = flip;
+document.getElementById("tile-5-back").onclick = flip;
+document.getElementById("tile-6-back").onclick = flip;
+document.getElementById("tile-7-back").onclick = flip;
+document.getElementById("tile-8-back").onclick = flip;
+document.getElementById("tile-9-back").onclick = flip;
+document.getElementById("tile-10-back").onclick = flip;
+document.getElementById("tile-11-back").onclick = flip;
+document.getElementById("tile-12-back").onclick = flip;
+document.getElementById("tile-13-back").onclick = flip;
+document.getElementById("tile-14-back").onclick = flip;
+document.getElementById("tile-15-back").onclick = flip;
+document.getElementById("tile-16-back").onclick = flip;
+document.getElementById("tile-17-back").onclick = flip;
+document.getElementById("tile-18-back").onclick = flip;
+document.getElementById("tile-19-back").onclick = flip;
+document.getElementById("tile-20-back").onclick = flip;
+document.getElementById("tile-21-back").onclick = flip;
+document.getElementById("tile-22-back").onclick = flip;
+document.getElementById("tile-23-back").onclick = flip;
+document.getElementById("tile-24-back").onclick = flip;
+document.getElementById("tile-25-back").onclick = flip;
 
 
 window.addEventListener('DOMContentLoaded', function () {
